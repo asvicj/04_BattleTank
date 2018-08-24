005375ba82e82804258d62db28bf334f93aad5c3
asvicj <masvicj@gmail.com>
2018-08-25 05:29:05 +0900

    BT 128

--- BattleTank/Intermediate/Build/Win64/UE4Editor/Inc/BattleTank/BattleTank.init.gen.cpp
+++ BattleTank/Intermediate/Build/Win64/UE4Editor/Inc/BattleTank/BattleTank.init.gen.cpp
@@ -19,8 +19,8 @@ void EmptyLinkFunctionForGeneratedCodeBattleTank_init() {}
 			static const UE4CodeGen_Private::FPackageParams PackageParams = {
 				"/Script/BattleTank",
 				PKG_CompiledIn | 0x00000000,
-				0xB3E4B831,
-				0x23E50B5D,
+				0x23DCB802,
+				0xE0DBF4E2,
 				nullptr, 0,
 				METADATA_PARAMS(nullptr, 0)
 			};
